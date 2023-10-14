cb12c64a9f1c7e9932a4bafe66cc0f21b9e6a60d
slavafive <vyacheslavefimov1999@gmail.com>
2023-10-14 12:17:21 +0200

    Basic form is implemented

--- api/api.py
+++ api/api.py
@@ -1,8 +1,15 @@
 import time
 from flask import Flask, request, jsonify
+from PIL import Image
 import json
+import os
+from io import BytesIO
+import requests
+from transformers import pipeline
 
 app = Flask(__name__, static_folder='../build', static_url_path='/')
+
+image_to_text = pipeline("image-to-text", model="Salesforce/blip-image-captioning-large")
 
 
 @app.errorhandler(404)
@@ -20,8 +27,30 @@ def get_current_time():
     return {'time': time.time()}
 
 
+def read_image(url):
+    response = requests.get(url)
+    image = Image.open(BytesIO(response.content))
+    return image
+
+
+def generate_text(url):
+    result = image_to_text(url)
+    generated_text = result[0]['generated_text']
+    return generated_text
+
+
 @app.route('/api/generate', methods=['POST'])
 def generate():
-    record = json.loads(request.data)
-    print(record)
-    return jsonify({'generated_text': 'some text'})
+    data = request.data
+    parsed_data = json.loads(data.decode('utf-8'))
+    urls = parsed_data['urls']
+    # image = read_image(urls[0])
+    generated_text = generate_text(urls[0])
+    return jsonify({'generated_text': generated_text})
+
+
+@app.route('/api/upload', methods=['POST'])
+def upload():
+    print(request.data)
+    file = request.files['file']
+    file.save(os.path.join('111.jpg'))
--- src/App.js
+++ src/App.js
@@ -1,19 +1,35 @@
-import React, { useState, useEffect } from "react";
-import { BrowserRouter, Link, Switch, Route } from "react-router-dom";
-import logo from "./logo.svg";
+import React, { useState, useRef } from "react";
 import "./App.css";
+import Form from "react-bootstrap/Form";
+import Button from "react-bootstrap/Button";
+
+import { useForm } from "react-hook-form";
 
 function App() {
-  const [currentTime, setCurrentTime] = useState(0);
   const [generatedText, setGeneratedText] = useState("");
   const [url, setUrl] = useState("");
+  const { register, handleSubmit } = useForm();
 
-  const handleSubmit = async (event) => {
-    event.preventDefault()
+  // const onSubmit2 = async (data) => {
+  //   const formData = new FormData();
+  //   formData.append("file", data.file);
+
+  //   const requestOptions = {
+  //     method: "POST",
+  //     body: formData
+  //   };
+  //   const result = await fetch("api/generate", requestOptions);
+  //   const response = await result.json();
+  //   setGeneratedText(response["generated_text"]);
+  // };
+
+  const onSubmit = async (event) => {
+    event.preventDefault();
+
     const requestOptions = {
       method: "POST",
       headers: { "Content-Type": "application/json" },
-      body: JSON.stringify({ url: url }),
+      body: JSON.stringify({ urls: [url, "876"] }),
     };
     const result = await fetch("api/generate", requestOptions);
     const data = await result.json();
@@ -23,19 +39,61 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <form onSubmit={handleSubmit}>
-          <label>
+        This app allows you to generate text for a given image that you can use
+        to create an Instagram post!
+        <br />
+        <br />
+        <Form onSubmit={onSubmit}>
+          {/* <Form onSubmit={handleSubmit(onSubmit2)}> */}
+          {/* <label>
             Enter image URL:
             <input
               type="text"
               value={url}
               onChange={(e) => setUrl(e.target.value)}
             />
-          </label>
-          <input type="submit" />
-        </form>
+          </label> */}
 
-        <p>Generated text: {generatedText}</p>
+          <Form.Group
+            controlId="exampleForm.ControlInput1"
+            value={url}
+            onChange={(e) => setUrl(e.target.value)}
+          >
+            <Form.Label>URL address</Form.Label>
+            <br />
+            <div>
+              <Form.Control
+                type="text"
+                placeholder="URL"
+              />
+            </div>
+          </Form.Group>
+
+          <br />
+
+          <Form.Group controlId="formFile" className="mb-3">
+            <Form.Label></Form.Label>
+            <Form.Control type="file" {...register("file")} />
+          </Form.Group>
+          <br />
+
+          <Button type="submit">Generate</Button>
+          <br />
+
+          <Form.Group className="mb-3" controlId="exampleForm.ControlTextarea1">
+            <Form.Label>Generated text</Form.Label>
+            <br />
+            <Form.Control
+              as="textarea"
+              rows={3}
+              value={generatedText}
+              onChange={(e) => setGeneratedText(e.target.value)}
+            />
+          </Form.Group>
+
+          {/* <input type="submit" /> */}
+        </Form>
+        {/* <p>Generated text: {generatedText}</p> */}
       </header>
     </div>
   );
